e630812fad5144878e2184cda28909bc32b0950c
Lakshyalamba <choudharylakshya758@gmail.com>
2025-12-01 17:20:14 +0530

    Added backend url

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,10 @@ import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3333;
-app.use(cors());
+app.use(cors({
+  origin: "https://moneymind-frontend.onrender.com",
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api', authRoutes);
 
